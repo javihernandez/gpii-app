5791a1f08101512e648ab75b297791af23ad933f
Javier Hernández <javi@raisingthefloor.org>
2018-07-13 14:13:36 +0200

    RESNA: Updated package.json and added version.txt

--- provisioning/Installer.ps1
+++ provisioning/Installer.ps1
@@ -63,6 +63,10 @@ Invoke-Command $npm "install" $projectDir
 # Currently required to generate the "mega" messages bundle
 Invoke-Command $npm "run build --prefix" $projectDir
 
+# GPII-2895: Create a txt file that includes the GPII version
+$txtVersionGenerator = Join-Path $projectDir "createVersionFile.js"
+Invoke-Command $node $txtVersionGenerator $projectDir
+
 # Copy all the relevant content of projectDir into preStaging
 # TODO: Make all these robocopies a bit more sexy
 Invoke-Command "robocopy" "..\node_modules $(Join-Path $preStagingDir "node_modules") /job:gpii-app.rcj *.*" $provisioningDir -errorLevel 3
@@ -76,6 +80,10 @@ Invoke-Command "robocopy" "$projectDir $preStagingDir index.js" $provisioningDir
 Invoke-Command "robocopy" "$projectDir $preStagingDir package.json" $provisioningDir -errorLevel 3
 Invoke-Command "robocopy" "$projectDir $preStagingDir package-lock.json" $provisioningDir -errorLevel 3
 Invoke-Command "robocopy" "$projectDir $preStagingDir README.md" $provisioningDir -errorLevel 3
+
+# GPII-2895: Copy version.txt into staging folder
+$stagingDir = Join-Path $installerDir "staging"
+Invoke-Command "robocopy" "$projectDir $stagingDir version.txt" $provisioningDir -errorLevel 3
 
 $packagerMetadata = "--app-copyright=`"Raising the Floor - International Association`" --win32metadata.CompanyName=`"Raising the Floor - International Association`" --win32metadata.FileDescription=`"GPII-App`" --win32metadata.OriginalFilename=`"gpii.exe`" --win32metadata.ProductName=`"GPII-App`" --win32metadata.InternalName=`"GPII-App`""
 
